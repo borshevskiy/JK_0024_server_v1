5190d23885b28b3ea70701400f1a1448a22c7814
borshevskiy <sk8er1001@mail.ru>
2022-04-28 00:46:30 +0600

    ready

--- app/src/main/java/com/template/LoadingActivity.kt
+++ app/src/main/java/com/template/LoadingActivity.kt
@@ -1,10 +1,13 @@
 package com.template
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.database.DataSnapshot
@@ -12,46 +15,74 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.template.MainActivity.Companion.IS_STARTED_UP
+import com.template.MainActivity.Companion.SERVER_URL
 import com.template.databinding.ActivityLoadingBinding
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import okhttp3.*
+import java.io.IOException
 import java.util.*
 
 class LoadingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoadingBinding
-    private lateinit var analytics: FirebaseAnalytics
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val preferences = getSharedPreferences(MainActivity.APP_SETTINGS, MODE_PRIVATE)
+        preferences = getSharedPreferences(MainActivity.APP_SETTINGS, MODE_PRIVATE)
 
-        analytics = Firebase.analytics
         Firebase.database.reference.addValueEventListener(object : ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
-//                preferences.edit().putBoolean(IS_STARTED_UP, true).apply()
-                var firebaseDomain = ""
-                val matcher = Patterns.WEB_URL.matcher(snapshot.value.toString())
-                if (matcher.find()) firebaseDomain = matcher.group()
-                val url = "$firebaseDomain/?packageid=${BuildConfig.APPLICATION_ID}&usserid=${UUID.randomUUID()}&getz=${TimeZone.getDefault().id}&getr=utm_source=google-play&utm_medium=organic"
+                val domain = snapshot.value.toString()
+                preferences.edit().putBoolean(IS_STARTED_UP, true).apply()
+                if (snapshot.value != null) getUrl(domain) else {
+                    startActivity(Intent(this@LoadingActivity, MainActivity::class.java))
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 startActivity(Intent(this@LoadingActivity, MainActivity::class.java))
             }
         })
+    }
 
-        try {
-            val test = OkHttpClient().newCall(Request.Builder()
-                .url("https://angryy.xyz/?packageid=com.template&usserid=e91ba3ec-6181-456d-a98d-4bbab7f1d2b8&getz=Asia/Omsk&getr=utm_source=google-play&utm_medium=organic")
-                .build()).execute().body.toString()
-            Log.d("TEST", test)
-        } catch (e: Exception) {
-
+    private fun getUrl(domain: String) {
+        val matcher = Patterns.WEB_URL.matcher(domain)
+        if (matcher.find()) {
+            FIREBASE_DOMAIN = matcher.group()
+            val url ="$FIREBASE_DOMAIN/?packageid=$PACKAGE_ID&usserid=$USER_ID&getz=$TIMEZONE_VALUE&getr=utm_source=google-play&utm_medium=organic"
+            getData(url)
         }
+    }
+
+    private fun getData(url: String) {
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("TEST", e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                when(response.code()) {
+                    403 -> startActivity(Intent(this@LoadingActivity, MainActivity::class.java))
+                    200 -> {
+                        preferences.edit().putString(SERVER_URL, response.body().toString()).apply()
+                        CustomTabsIntent.Builder().build().launchUrl(this@LoadingActivity, Uri.parse(response.body().toString()))
+                    }
+                }
+            }
+        })
+    }
+
+    companion object {
+        private var FIREBASE_DOMAIN = ""
+        private const val PACKAGE_ID = BuildConfig.APPLICATION_ID
+        private val TIMEZONE_VALUE = TimeZone.getDefault().id
+        private var USER_ID = UUID.randomUUID()
     }
 }
--- app/src/main/java/com/template/MainActivity.kt
+++ app/src/main/java/com/template/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.template
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.template.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,22 +16,27 @@ class MainActivity : AppCompatActivity() {
     companion object{
         const val APP_SETTINGS = "App settings"
         const val IS_STARTED_UP = "Is started up"
+        const val SERVER_URL = "server url"
     }
-//    private lateinit var FIREBASE_URL: String
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var analytics: FirebaseAnalytics
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val preferences = getSharedPreferences(APP_SETTINGS, MODE_PRIVATE)
+        preferences = getSharedPreferences(APP_SETTINGS, MODE_PRIVATE)
+        analytics = Firebase.analytics
 
-        if (!preferences.contains(IS_STARTED_UP)) {
+        if (preferences.contains(SERVER_URL)) {
+            CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(preferences.getString(SERVER_URL, "")))
+        } else if (!preferences.contains(IS_STARTED_UP)) {
             val intent = Intent(this, LoadingActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
-//        CustomTabsIntent.Builder().build().launchUrl(this, Uri.parse(FIREBASE_URL))
     }
 }
