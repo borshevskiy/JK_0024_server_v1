b7d8b825819960d95aeda6e3a911b38fe848b903
borshevskiy <sk8er1001@mail.ru>
2022-06-13 14:56:40 +0600

    75% ready

--- app/src/main/java/com/template/MainActivity.kt
+++ app/src/main/java/com/template/MainActivity.kt
@@ -2,10 +2,10 @@ package com.template
 
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.browser.customtabs.CustomTabsIntent
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.template.databinding.ActivityMainBinding
 
@@ -15,18 +15,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var preferences: SharedPreferences
     private lateinit var analytics: FirebaseAnalytics
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         preferences = getSharedPreferences(APP_SETTINGS, MODE_PRIVATE)
 
         if (internetAvailable(this)) {
             analytics = FirebaseAnalytics.getInstance(this)
             if (preferences.contains(SERVER_URL)) {
-                CustomTabsIntent.Builder().setToolbarColor(R.color.black).build().launchUrl(this, Uri.parse(preferences.getString(SERVER_URL, "")))
-                finish()
+                startActivity(Intent(this, WebActivity::class.java))
             } else if (!preferences.contains(IS_STARTED_UP)) {
                 val intent = Intent(this, LoadingActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
--- app/src/main/java/com/template/WebActivity.kt
+++ app/src/main/java/com/template/WebActivity.kt
@@ -1,11 +1,27 @@
 package com.template
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.template.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web)
+        setTheme(R.style.Theme_NoActionBar)
+        binding = ActivityWebBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        with(binding.webView) {
+            settings.javaScriptEnabled = true
+            settings.domStorageEnabled = true
+            getSharedPreferences(APP_SETTINGS, MODE_PRIVATE).getString(SERVER_URL, "")?.let { loadUrl(it) }
+        }
     }
 }
